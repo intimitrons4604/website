f1f72dbd464eb6c2cc419db6600e8647cf321c8c
Tami Miguens <tami@ripeda.com>
2017-11-19 22:30:52 -0700

    Finish off the initial home page for more pages to come later.

---
+++ header.php
@@ -0,0 +1,46 @@
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-grid.css">
+	<link rel="stylesheet" href="css/bootstrap-reboot.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/animate.css">
+
+    <script src="https://use.fontawesome.com/acdf66c231.js"></script>
+	  
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=1933454553572459';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	  
+  </head>
+  
+  
+  <body>
+	  
+	<nav class="navbar navbar-expand-lg navbar-dark trons-purple-bkgnd sticky-top">
+	  <a class="navbar-brand" href="#">
+   	 	<img  src="images/Logo-header-white.svg" height="65">
+ 	  </a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarContent">
+		<ul class="navbar-nav ml-auto">
+		  <li <?php echo ($page == 'index.php') ? "class='nav-item active'" : "class='nav-item'"; ?>>
+			<a class="nav-link" href="index.php">Home</a>
+		  </li>
+		  <li <?php echo ($page == 'team.php') ? "class='nav-item active'" : "class='nav-item'"; ?>>
+			<a class="nav-link" href="#">Team</a>
+		  </li>
+		  <li <?php echo ($page == 'sponsor.php') ? "class='nav-item active'" : "class='nav-item'"; ?>>
+			<a class="nav-link" href="#">Sponsorship</a>
+		  </li>
+		</ul>
+	  </div>
+	</nav>
---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Intimitrons Demo</title>
+
+<?php $page = "index.php" ; include_once('header.php');?>
+
+	<div class="jumbotron-fluid">
+		
+		<div class="container-fluid remove-padding">
+   
+			<div class="row">
+				
+				<div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-sm-12 remove-padding">
+					<div class="hero" style="background-image:url('https://farm5.staticflickr.com/4185/33682601574_0574678c36_h.jpg')">
+
+					</div>
+				</div>
+				<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-sm-12 hero-sidebar remove-padding">
+					<img class="animated zoomIn" width="200" height="200" src="images/Logo-Alien-Only.svg">
+					<div class="hero-sidebar-text">
+						 <h1 class="pt-5 pb-2">4604</h1>
+						 <h2 class="pt-0"> <i class="fa fa-heart animated pulse"></i></h2> 
+						 <h2>robots</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+    
+	<div class="container-fluid trons-green-bkgnd hero-banner">
+		<div class="row align-items-center pl-4 pt-4 pb-3">
+			<div class="col-xl-9 col-lg-9 col-md-6 col-sm-6 col-xs-12">
+				<h2>The Intimitrons from AREA 51</h2>
+				<h5>Western Canada's First All-Girls FRC Robotics Team</h5>
+			</div>
+			<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-12 text-center">
+				<button class="medium-button trons-purple-bkgnd">BECOME A TRON</button>
+			</div>
+		</div>
+	  </div>
+	 
+	<div class="container-fluid">  
+
+		<div class="row pl-5 pr-5 pt-5 pb-5">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<h2>5 Years of Learning STEM</h2>
+				<p>The Intimitrons From Area 51 is one of the few all girls FIRST Robotics teams in Canada. We are the first all-female team in Western Canada! Our objective is to encourage female participation in technical vocations- fields that are currently male dominated.  By creating robots to compete in FIRST competitions, we are able to accomplish this. The Intimitrons team was established in 2012, and this 2017 season was our fifth!</p>
+		   </div>
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<div class="container">
+					<div id="season-photos-carousel" class="carousel slide" data-ride="carousel">
+					  <div class="carousel-inner" role="listbox">
+						<div class="carousel-item active">
+						  <img class="d-block w-100" src="https://farm5.staticflickr.com/4164/33682750284_a97cb6862b_k.jpg">
+						</div>
+						<div class="carousel-item">
+						  <img class="d-block w-100" src="https://farm5.staticflickr.com/4170/34484137066_dbfe878a0b_k.jpg">
+						</div>
+						<div class="carousel-item">
+						  <img class="d-block w-100" src="https://farm5.staticflickr.com/4253/35692617622_36c937a3c2_k.jpg">
+						</div>
+					  </div>
+					  <a class="carousel-control-prev" href="#season-photos-carousel" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					  </a>
+					  <a class="carousel-control-next" href="#season-photos-carousel" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					  </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container-fluid trons-purple-bkgnd">
+		
+		<div class="row p-5">
+			
+			<div class="col-12">
+
+				<h5>Sign up for our newsletter here, if you’d like updates on what we’ve been up to!</h5>
+				<form action="https://intimitrons.us3.list-manage.com/subscribe/post?u=d479728f08b6043f1d957d5b1&amp;id=cc9ef4518a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				  <div class="form-row align-items-center">
+					<div class="col-sm-2">
+					  <label class="sr-only" for="mce-FNAME">First Name</label>
+					  <input type="text" value="" name="FNAME" id="mce-FNAME" class="form-control mb-2 mb-sm-0" placeholder="Jane">
+					</div>
+					<div class="col-sm-2">
+					  <label class="sr-only" for="mce-LNAME">Last Name</label>
+					  <input type="text" value="" name="LNAME" id="mce-LNAME" class="form-control mb-2 mb-sm-0" placeholder="Doe">					
+					</div>
+					<div class="col-sm-4">
+					  <label class="sr-only" for="mce-EMAIL">Email</label>
+					  <div class="input-group mb-2 mb-sm-0">
+						<div class="input-group-addon"><i class="fa fa-envelope"></i></div>
+						<input type="email" value="" name="EMAIL" id="mce-EMAIL" class="required form-control" id="newsletter-email" placeholder="Email">
+					  </div>
+					</div>
+					<div class="col-auto">
+					  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d479728f08b6043f1d957d5b1_cc9ef4518a" tabindex="-1" value=""></div>
+					  <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn trons-green-button">Subscribe</button>
+					</div>
+				  </div>
+				</form>
+			</div>
+		</div>
+	</div>
+    
+    
+    
+    <div class="parallax"></div>
+    
+    <?php include_once('footer.php');?>
+ 

1a26c39b4f37d43717a0641783c49bb2c6d45e46
pgfisico <pgfisico.dev@gmail.com>
2020-06-05 22:56:05 -0600

    Add favicons (#75)
    Resolves #74

--- next/src/components/page.js
+++ next/src/components/page.js
@@ -5,6 +5,10 @@ import PropTypes from 'prop-types'
 import { Footer } from './footer.js'
 import { NavBar } from './navbar.js'
 
+import favicon_ico from '../images/favicon/favicon.ico'
+import favicon_png16 from '../images/favicon/favicon-16x16.png'
+import favicon_png32 from '../images/favicon/favicon-32x32.png'
+
 import '../sass/main.scss'
 
 export const Page = ({ activePage, children, title }) => {
@@ -12,6 +16,9 @@ export const Page = ({ activePage, children, title }) => {
     <>
       <Helmet titleTemplate='%s | Intimitrons From Area 51'>
         <title>{title}</title>
+        <link rel='icon' type='image/vnd.microsoft.icon' href={favicon_ico} />
+        <link rel='icon' type='image/png' sizes='32x32' href={favicon_png32} />
+        <link rel='icon' type='image/png' sizes='16x16' href={favicon_png16} />
       </Helmet>
       <NavBar activePage={activePage} />
       <main>{children}</main>
